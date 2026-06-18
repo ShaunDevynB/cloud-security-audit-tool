f209f8560acbaefcd99964010394bb78913027e1
ShaunDevynB <shaundarko1@gmail.com>
2026-06-18 15:33:09 -0400

    Initial commit: Spring Boot project structure generated

---
+++ src/main/java/com/shaundevynb/cspm/CloudSecurityAuditToolApplication.java
@@ -0,0 +1,13 @@
+package com.shaundevynb.cspm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSecurityAuditToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSecurityAuditToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaundevynb/cspm/CloudSecurityAuditToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaundevynb.cspm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSecurityAuditToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
